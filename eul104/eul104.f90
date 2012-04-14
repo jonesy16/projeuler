06002e5243092356c02f7f77555922fff92d5cfb
Eric N. Jones <eric.jones@transware.net>
2012-04-14 15:50:33 -0500

    Further modifications to routines which resulted in no performance boost.

--- eul104/eul104.f90
+++ eul104/eul104.f90
@@ -15,15 +15,16 @@ PROGRAM PANCHECK
   ! dataArray(:,1) = first fib. number
   ! dataArray(:,2) = second fib. number
   ! dataArray(:,3) = sum of first and second fib. numbers
+  ! dataArray(:,4) = temporary array for math operations
   Integer, Parameter :: ARRAYSIZE=100000
-  Integer*4, Dimension (1:ARRAYSIZE,1:3) :: dataArray
+  Integer*4, Dimension (1:ARRAYSIZE,1:4) :: dataArray
   Integer*4 :: seqNumber, i, j, pandigitSeqCount, maxIndex, loopCount, numTens, forceFullsum
   dataArray(:,:)=0
   dataArray(1,1)=1
   dataArray(1,2)=1
   dataArray(1,3)=2
   seqNumber=2
-  maxIndex=1
+  maxIndex=10
   forceFullsum=0
   do
     seqNumber=seqNumber+1
@@ -31,44 +32,33 @@ PROGRAM PANCHECK
     ! Since the arrays were summed, some of the array elements may be
     ! greater than 10, these need to be properly carried over to the next
     ! higher element
-    do i=1, 9, 1
-      if ( dataArray(i,3) >= 10 ) then
-        dataArray(i,3)=dataArray(i,3)-10
-        dataArray(i+1,3)=dataArray(i+1,3)+1
-        if (i == maxIndex) then
-          maxIndex=maxIndex+1
-          exit
-        end if
-      end if
-    end do
+    dataArray(2:10,4)=dataArray(1:9,3)/10
+    dataArray(1:9,3)=mod(dataArray(1:9,3),10)
+    dataArray(1:10,3)=sum(dataArray(1:10,3:4),dim=2)
     if ( dataArray(10,3) > 100000000 ) then
        forceFullsum = 1
     end if
     ! Check last 9 digits for pandigital status
     pandigitSeqCount=0
-    do i=1, 9
-      do j=1, 9
-        if ( dataArray(j,3) == 0 ) then
-          exit
-        else if ( dataArray(j,3) == i ) then
-          pandigitSeqCount=pandigitSeqCount+1
-          exit
-        end if
+    if (maxval(dataArray(1:9,3)) == 9 .and. minval(dataArray(1:9,3)) == 1 ) then
+      do i=1, 9
+        do j=1, 9
+          if ( dataArray(j,3) == i ) then
+            pandigitSeqCount=pandigitSeqCount+1
+            exit
+          end if
+        end do
       end do
-    end do
+    end if
     if ( pandigitSeqCount == 9 ) then
       write (*,'(A,I0,A)') " F_", seqNumber, " contains a pandigital number in the last 9 digits"
-      ! Now that we know that the first 9 digits are pandigital, we need to
-      ! compute our fib. sum
       do i=1, ARRAYSIZE, 1
         if ( dataArray(i,3) >= 10 ) then
           numTens= dataArray(i,3)/10
           dataArray(i,3)=mod(dataArray(i,3),10)
           dataArray(i+1,3)=dataArray(i+1,3)+numTens
           if (i == maxIndex) then
-            !print *, "changing max index from ", maxIndex, " to "
             maxIndex=maxIndex+log10(real(numTens)) + 1
-            !print *, maxIndex, " based on ", numTens
           end if
           if (i == maxIndex) exit
         end if
@@ -76,33 +66,29 @@ PROGRAM PANCHECK
       end do
       ! Check first 9 digits for pandigital status
       pandigitSeqCount=0
-      do i=1, 9, 1
-        do j=maxIndex, maxIndex-8, -1
-          if ( dataArray(j,3) == 0 ) then
-            exit
-          else if ( dataArray(j,3) == i ) then
-            pandigitSeqCount=pandigitSeqCount+1
-            exit
-          end if
+      if (maxval(dataArray(maxIndex-8:maxIndex,3)) == 9 .and. minval(dataArray(maxIndex-8:maxIndex,3)) == 1 ) then
+        do i=1, 9, 1
+          do j=maxIndex-8, maxIndex, 1
+            if ( dataArray(j,3) == i ) then
+              pandigitSeqCount=pandigitSeqCount+1
+              exit
+            end if
+          end do
         end do
-      end do
+      end if
       if ( pandigitSeqCount == 9 ) then
         write (*,'(A,I0,A)') " F_", seqNumber, " contains a pandigital number in the first and last 9 digits"
         write (*,'(A,I0,A)') " This number is ", maxIndex ," digits long"
-        stop
+        exit
       end if
     else if ( forceFullsum == 1 ) then
-      ! Now that we know that the first 9 digits are pandigital, we need to
-      ! compute our fib. sum
       do i=1, ARRAYSIZE, 1 
         if ( dataArray(i,3) >= 10 ) then
           numTens= dataArray(i,3)/10
           dataArray(i,3)=mod(dataArray(i,3),10)
           dataArray(i+1,3)=dataArray(i+1,3)+numTens
           if (i == maxIndex) then
-            !print *, "changing max index from ", maxIndex, " to "
             maxIndex=maxIndex+log10(real(numTens)) + 1
-            !print *, maxIndex, " based on ", numTens
           end if
           if (i == maxIndex) exit
         end if
@@ -112,13 +98,5 @@ PROGRAM PANCHECK
     dataArray(1:maxIndex,1)=dataArray(1:maxIndex,2)
     dataArray(1:maxIndex,2)=dataArray(1:maxIndex,3)
     dataArray(1:maxIndex,3)=sum(dataArray(1:maxIndex,1:2),dim=2)
-    !dataArray(1:9,1)=dataArray(1:9,2)
-    !dataArray(1:9,2)=dataArray(1:9,3)
-    !dataArray(1:9,3)=sum(dataArray(1:9,1:2),dim=2)
-    !if (loopCount == 10) then
-      !print *, dataArray(1:maxIndex,1)
-      !print *, dataArray(1:maxIndex,2)
-      !print *, dataArray(1:maxIndex,3)
-    !end if
   end do
 end Program
